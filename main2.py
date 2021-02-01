76c4b95fe04a562a85b773b74695525e6d40b85e
gwenhall <gwenhall@iu.edu>
2021-02-01 16:10:37 -0500

    Completed guessing game assignment

--- main2.py
+++ main2.py
@@ -1,3 +1,16 @@
 #!/usr/bin/env python3
-import sys
+import sys, random
 assert sys.version_info >= (3,9), "This script requires at least Python 3.9"
+number = random.randrange(1,10)
+count = 0
+guess = -1
+while guess != number:
+    count = count + 1
+    guess = input("Guess a number from 1 to 10: ")
+    try:
+        guess = int(guess)
+        if guess != number:
+            print("Try again!")
+    except: 
+        print("Please enter a number.")
+print("Great job! You got it!")
